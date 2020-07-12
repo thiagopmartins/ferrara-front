361ac0682f35a03215101010066f97d1d91568bd
unknown <thiagopmartins@outlook.com>
2020-07-12 10:20:50 -0300

    ajsutes de fonte e mascara

--- src/app/dashboard/orders/orders.module.ts
+++ src/app/dashboard/orders/orders.module.ts
@@ -1,10 +1,10 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { NgxMaskModule } from "ngx-mask";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgxMaskModule } from 'ngx-mask';
 
-import { OrdersComponent } from "./orders.components/orders.component";
-import { OrdersRoutingModule } from "./orders-routing.module";
-import { SharedModule } from "src/app/shared/shared.module";
-import { CurrencyMaskModule } from "ng2-currency-mask";
+import { OrdersComponent } from './orders.components/orders.component';
+import { OrdersRoutingModule } from './orders-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { NgxPrintModule } from 'ngx-print';
 
 @NgModule({
